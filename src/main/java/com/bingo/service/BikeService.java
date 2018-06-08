846d1499e32bfacfc722102476dac989419945e8
bingooo <szuyanglb@outlook.com>
2018-06-08 11:25:11 +0800

    利用mongodb保存bike数据

---
+++ src/main/java/com/bingo/service/BikeService.java
@@ -0,0 +1,9 @@
+package com.bingo.service;
+
+import com.bingo.model.Bike;
+
+public interface BikeService {
+	
+	void save(Bike bike);
+	
+}
---
+++ src/main/java/com/bingo/service/BikeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.bingo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+
+import com.bingo.model.Bike;
+
+@Service
+public class BikeServiceImpl implements BikeService {
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
+	@Override
+	public void save(Bike bike) {
+		// 将bike数据添加到mongodb中的bikes collection中
+		mongoTemplate.insert(bike, "bikes");
+	}
+	
+}
