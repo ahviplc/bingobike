bf5f4add21f96b81b321df0529618de39b38b650
bingooo <szuyanglb@outlook.com>
2018-06-09 15:07:36 +0800

    geoHash实现附近bike查找

--- src/main/java/com/bingo/controller/BikeController.java
+++ src/main/java/com/bingo/controller/BikeController.java
@@ -3,9 +3,13 @@ package com.bingo.controller;
 import com.bingo.model.Bike;
 import com.bingo.service.BikeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.GeoResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = {"/bike"})
@@ -18,5 +22,10 @@ public class BikeController {
     public String addBike(Bike bike) {
         bikeService.save(bike);
         return "success";
+    }
+
+    @GetMapping(value = {"/findNear"})
+    public List<GeoResult<Bike>> findNear(double longitude, double latitude) {
+        return bikeService.findNear(longitude, latitude);
     }
 }
--- src/main/java/com/bingo/model/Bike.java
+++ src/main/java/com/bingo/model/Bike.java
@@ -1,6 +1,8 @@
 package com.bingo.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
+import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -8,8 +10,10 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Bike {
     @Id
     private String id;
-    private double longitude;
-    private double latitude;
+
+    // 表示经纬度的数组[经度, 纬度]
+    @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
+    private double[] location;
 
     @Indexed
     private Long bikeNo;
@@ -24,20 +28,12 @@ public class Bike {
         this.id = id;
     }
 
-    public double getLongitude() {
-        return longitude;
+    public double[] getLocation() {
+        return location;
     }
 
-    public void setLongitude(double longitude) {
-        this.longitude = longitude;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
-        this.latitude = latitude;
+    public void setLocation(double[] location) {
+        this.location = location;
     }
 
     public Long getBikeNo() {
